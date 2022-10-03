a09517ecc135226be5f82713d736e3f640bc84fe
+ <+>
2022-10-03 21:48:28 +0300

    complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,68 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  //current state - firstName
+  //setFirst - function that is used to update our state.
+  //useState - set the initial state to an empty string
+  const [firstName, setFirst] = useState("");
+  const [lastName, setLast] = useState("");
+  const [paragraph,setParagraph]  = useState([]);
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function nameSubmit  (e){
+    e.preventDefault();
+      if(firstName&&lastName){
+        setFirst("")
+        setLast("")
+        //takes the string of firstname and lastname then returns both
+        setParagraph(()=>[firstName,lastName])
+      }
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+    <div>
+    <form onSubmit={nameSubmit}>
+      <label>
+        First Name:
+        <input
+          value={firstName}
+          name="firstName"
+
+          //onchange event occurs when the value of an element has been changed.
+          onChange={(e) => {
+             console.log(e.target.value);
+            setFirst(e.target.value);
+          }}
+          type="text"
+          autoComplete="off"
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          value={lastName}
+          name="lastName"
+          onChange={(e) => {
+             console.log(e.target.value);
+            setLast(e.target.value);
+          }}
+          type="text"
+          autoComplete="off"
+        />
+      </label>
+      <button type="submit" >
+        Submit
+      </button>
     </form>
+
+    {
+      paragraph.map((paragraph)=><div>
+        <p>{paragraph}  </p>
+      </div>)
+    }
+    </div>
+    //map() function iterates over an array
+    //and manipulate or change data items.
   );
 }
-
 export default Form;
